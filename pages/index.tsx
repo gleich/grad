917d05b30098927fee0ba45d34b0931d9a298a78
Matt Gleich <git@mattglei.ch>
2022-06-08 16:47:57 -0400

    fix: size
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,17 @@ import Confetti from 'react-confetti';
 const Home: NextPage = () => {
   const { width, height } = useWindowSize();
   return (
-    <div>
-      <Confetti width={width} height={height} />
+    <div className={styles.main}>
+      <Confetti width={2000} height={2000} numberOfPieces={1000} />
       <div className={styles.body}>
         <h1>We&apos;re graduating!</h1>
         <h1>Goffstown Class of 2022</h1>
         <p>
           - A small website by <a href="https://mattglei.ch">Matt Gleich</a>
         </p>
+        <a href="https://photos.mattglei.ch/ghs">
+          <button>Checkout the photos I took this year</button>
+        </a>
       </div>
     </div>
   );
