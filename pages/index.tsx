a56aa4f01a1fc8820db4b4d74b8c2a416f99d631
Matt Gleich <git@mattglei.ch>
2022-06-08 16:34:45 -0400

    feat: confetti
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
 
 const Home: NextPage = () => {
+  const { width, height } = useWindowSize();
   return (
-    <div className={styles.body}>
-      <h1>We&apos;re graduating!</h1>
-      <h1>Goffstown Class of 2022</h1>
+    <div>
+      <Confetti width={width} height={height} />
+      <div className={styles.body}>
+        <h1>We&apos;re graduating!</h1>
+        <h1>Goffstown Class of 2022</h1>
+        <p>
+          - A small website by <a href="https://mattglei.ch">Matt Gleich</a>
+        </p>
+      </div>
     </div>
   );
 };
