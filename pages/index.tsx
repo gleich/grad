478e7096ab1e68baf187a207decab85f5cd17b93
Matt Gleich <git@mattglei.ch>
2022-06-08 16:55:12 -0400

    feat: rick roll
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
           - A small website by <a href="https://mattglei.ch">Matt Gleich</a>
         </p>
         <a
-          href="https://photos.mattglei.ch/ghs"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noreferrer"
         >
